3bb4f1371860224df6fb9043c9a4228641924ca0
Javier Candalaft <jc@nybblegroup.com>
2016-07-18 17:20:11 -0300

    Adapting to new ng cli architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { FieldComponent } from './field/field.component';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [FieldComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/field/field.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-field',
+  templateUrl: 'field.component.html',
+  styleUrls: ['field.component.css']
+})
+export class FieldComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
